6a3edc87da6b2d09cf1e0009c0b239b9d2f284cd
Garrett <AdultVHS@gmail.com>
2023-08-10 23:53:12 -0500

    comment lines

--- scripts.js
+++ scripts.js
@@ -1,3 +1,23 @@
-fetch('https://jsonplaceholder.typicode.com/posts')
-    .then((response)=> response.json())
-    .then((json)=>console.log(json));
+//create new variable and add new XML obj to it
+let request = new XMLHttpRequest()
+
+//open a new connection using the GET req on the URL endpoint
+request.open('GET', 'https://jsonplaceholder.typicode.com/posts', true)
+
+request.onload = function(){
+    //acces json data
+}
+
+//send req
+request.send()
+
+//alt, using fetch API and Async/await
+// function getData(){
+//     const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+//     const data = await response.json()
+// }
+
+
+// fetch('https://jsonplaceholder.typicode.com/posts')
+//     .then((response)=> response.json())
+//     .then((json)=>console.log(json));
